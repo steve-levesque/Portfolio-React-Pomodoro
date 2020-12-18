8643f37f84d75759cc92788a1aefd9e72c9deffc
Steve Lévesque <42849270+steve-levesque@users.noreply.github.com>
2020-12-17 22:50:57 -0500

    Updates

---
+++ precache-manifest.4725c8bffa80059d0f03675a3f95a976.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d449023576b71def6853",
+    "url": "/Portfolio-React-Pomodoro/static/css/main.7556affd.chunk.css"
+  },
+  {
+    "revision": "d449023576b71def6853",
+    "url": "/Portfolio-React-Pomodoro/static/js/main.d4490235.chunk.js"
+  },
+  {
+    "revision": "f389ec335c3294cbb6c6",
+    "url": "/Portfolio-React-Pomodoro/static/js/1.f389ec33.chunk.js"
+  },
+  {
+    "revision": "8d93e69bf135c317c9c2",
+    "url": "/Portfolio-React-Pomodoro/static/js/runtime~main.8d93e69b.js"
+  },
+  {
+    "revision": "6e0dcab520f12f85a83a3f20826e7adf",
+    "url": "/Portfolio-React-Pomodoro/static/media/sounds_of_city.6e0dcab5.mp3"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Portfolio-React-Pomodoro/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "08f7751d76b940851a98b20daca7b93c",
+    "url": "/Portfolio-React-Pomodoro/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Portfolio-React-Pomodoro/precache-manifest.4725c8bffa80059d0f03675a3f95a976.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Portfolio-React-Pomodoro/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
